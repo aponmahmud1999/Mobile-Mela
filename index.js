f06150dcb6a16c9cabea399b17c285705e7f82c3
aponmahmud1999 <aponmahmud36@gmail.com>
2022-03-02 14:00:44 +0600

    show results

---
+++ index.js
@@ -0,0 +1,31 @@
+const search =()=>{
+    const text = document.getElementById('search-box');
+    const url = `https://openapi.programming-hero.com/api/phones?search=${text.value.toLowerCase()}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showphones(data.data, text))
+} 
+const showphones =(phones,text)=>{
+     const body = document.getElementById('cards')
+     body.textContent = ''
+     text.value = ''
+     document.getElementById('details').textContent = ''
+    if (phones.length == 0){
+      alert('no result found')
+    }
+    
+    const newp = phones.slice(0, 20)
+    newp.forEach(phone => {
+        const div = document.createElement('div')
+        div.innerHTML = `<div class="card bg-primary p-2 text-dark bg-opacity-10">
+    <img  src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
+    <div class="card-body">
+        <h5 class="card-title"><strong>${phone.phone_name}</strong></h5>
+        <p class="card-text"><strong>Brand: ${phone.brand}</strong></p>
+        <button type="button" class="btn btn-primary px-5 container"onclick="seemore('${phone.slug}')">See More</button>
+    </div>
+</div>` 
+        body.appendChild(div)
+        console.log(phone)
+    });
+}
