201a5d02e083881ada765abebd3a1ceb856c8281
aponmahmud1999 <aponmahmud36@gmail.com>
2022-03-02 22:25:56 +0600

    final submit

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ const search =()=>{
         .then(res => res.json())
         .then(data => showphones(data.data, text))
 } 
-const showphones =(phones,text)=>{
+const showphones =(mobiles,text)=>{
      const body = document.getElementById('cards')
      body.textContent = ''
      text.value = ''
      document.getElementById('details').textContent = ''
-    if (phones.length == 0){
-      alert('no result found')
+    if (mobiles.length == 0){
+      alert('no phones')
     }
     
-    const newp = phones.slice(0, 20)
+    const newp = mobiles.slice(0, 20)
     newp.forEach(phone => {
         const div = document.createElement('div')
-        div.innerHTML = `<div class="card bg-primary p-2 text-dark bg-opacity-10">
+        div.innerHTML = `<div class="card ">
     <img  src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
     <div class="card-body">
         <h5 class="card-title"><strong>${phone.phone_name}</strong></h5>
         <p class="card-text"><strong>Brand: ${phone.brand}</strong></p>
-        <button type="button" class="btn btn-primary px-5 container"onclick="showdetailfetch('${phone.slug}')">See More</button>
+        <button type="button" class="btn btn-primary  container"onclick="showdetailfetch('${phone.slug}')">See More</button>
     </div>
 </div>` 
         body.appendChild(div)
@@ -30,20 +30,43 @@ const showphones =(phones,text)=>{
 }
 const showdetailfetch=key=>{
     const text = document.getElementById('search-box');
-    console.log(key)
+  
     const url = `https://openapi.programming-hero.com/api/phone/${key}`
     fetch(url)
         .then(res => res.json())
         .then(data => showdetail(data.data))
 }
-
+const checkattributes=mobile=>{
+    
+    if(mobile.mainFeatures.memory==undefined||mobile.mainFeatures.memory=='')
+    mobile.memory='unknown'
+    if(mobile.releaseDate==undefined||mobile.releaseDate==='')
+    mobile.releaseDate='unknown'
+    if(mobile.mainFeatures.chipSet==undefined||mobile.mainFeatures.chipSet=='')
+    mobile.mainFeatures.chipSet='unknown'
+    if(mobile.others==undefined||mobile.others=='')
+    mobile.others=''
+}
 const showdetail=mobile=>{
+    document.getElementById('details').textContent=''
     const div = document.createElement('div')
+    checkattributes(mobile)
+    let txt = "";
+    for (const [key, value] of Object.entries(mobile.others))
+        txt+=`${key}: ${value}`;
+        txt=txt||'unknown'
         div.innerHTML = `<div class="card bg-primary p-2 text-dark bg-opacity-10">
     <img  src="${mobile.image}" class="card-img-top w-50 mx-auto" alt="...">
     <div class="card-body">
-        <h5 class="card-title"><strong>${mobile.phone_name}</strong></h5>
+    <h4 class="card-title"><strong>${mobile.name}</strong></h4>
         <p class="card-text"><strong>Brand: ${mobile.brand}</strong></p>
+        <h5 class="card-title"><strong>Features</strong></h5>
+        <p class="card-text"><strong>Memory: ${mobile.mainFeatures.memory}</strong></p>
+        <p class="card-text"><strong>Chipset: ${mobile.mainFeatures.chipSet}</strong></p>
+        <p class="card-text"><strong>Display Size: ${mobile.mainFeatures.displaySize}</strong></p>
+        <p class="card-text"><strong>Release date: ${mobile.releaseDate}</strong></p>
+        <p class="card-text"><strong>Sensors: ${mobile.mainFeatures.sensors}</strong></p>
+        <p class="card-text"><strong>Others: ${txt}</strong></p>
     </div>
 </div>` 
 document.getElementById('details').appendChild(div)
